042da461718e3863f63137ad234c2e8aba4f8657
Alkiasan <62717881+Alkiasan@users.noreply.github.com>
2024-07-09 05:48:11 +0100

    Edit Lily Registry

--- src/main/java/net/mcreator/horizonrpmod/init/HorizonrpmodModItems.java
+++ src/main/java/net/mcreator/horizonrpmod/init/HorizonrpmodModItems.java
@@ -15,7 +15,7 @@ import net.mcreator.horizonrpmod.HorizonrpmodMod;
 
 public class HorizonrpmodModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, HorizonrpmodMod.MODID);
-	public static final RegistryObject<Item> LILY_PLUSH = REGISTRY.register("lily_plush", () -> new LilyPlushItem());
+	public static final RegistryObject<Item> LILYPLUSH = REGISTRY.register("lilyplush", () -> new LilyPlushItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }

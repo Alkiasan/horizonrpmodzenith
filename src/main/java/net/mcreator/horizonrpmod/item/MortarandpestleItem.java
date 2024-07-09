548fc916ca8e7ad0096673c502158b3696912881
Alkiasan <jenge@LAPTOP-VA6BG0FO>
2024-07-09 12:09:10 -0500

    added recipes for void essence and mortar+pestle. Altered mortar+pestle behavior so it is not consumed upon crafting.

--- src/main/java/net/mcreator/horizonrpmod/item/MortarandpestleItem.java
+++ src/main/java/net/mcreator/horizonrpmod/item/MortarandpestleItem.java
@@ -2,10 +2,21 @@
 package net.mcreator.horizonrpmod.item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 
 public class MortarandpestleItem extends Item {
 	public MortarandpestleItem() {
 		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		return new ItemStack(this);
 	}
 }
